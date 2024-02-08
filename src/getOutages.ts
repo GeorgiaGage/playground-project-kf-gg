5042cf48e41f3d6315928b8bfe7339f420ea3c27
Georgia Gage <georgia.gage@accenture.com>
2024-02-08 11:21:11 +0000

    Setup jest and add error handling to basic API call

--- src/getOutages.ts
+++ src/getOutages.ts
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios, { AxiosError, HttpStatusCode } from "axios";
 
 // TODO: move the API key somewhere more secure
 // TODO: change file structure of API client
@@ -11,16 +11,24 @@ const instance = axios.create({
   },
 });
 
-function getOutages(): void {
+// This should do the bare minimum to wrap around the API
+async function getOutages(): Promise<
+  { data: {}; status: HttpStatusCode } | AxiosError | Error
+> {
   console.log("Fetching outages...");
-  instance
-    .get("/outages")
-    .then((response) => {
-      console.log(response.data);
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+  try {
+    const response = await instance.get("/outages");
+    console.log('Response: ', response.data);
+    return { data: response.data, status: response.status };
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      console.log('Axios Error: ', error.message);
+      return error;
+    } else {
+      console.log('Error: ', error);
+      return new Error("Something went wrong fetching outages");
+    }
+  }
 }
 
 export default getOutages;
